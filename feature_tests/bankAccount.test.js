3921cecf60ee2bf115edf06f9d329b94076bbc23
Tom O'Neill <tomoneill32@gmail.com>
2022-06-21 18:06:32 +0100

    test refactor

--- feature_tests/bankAccount.test.js
+++ feature_tests/bankAccount.test.js
@@ -3,32 +3,33 @@ const Transaction = require('../bankTransaction')
 
 describe('BankAccount', () => {
   describe('printStatement', () => {
+    let account;
+
+    beforeEach(() => {
+      account = new BankAccount();
+    })
+
     it('returns an empty statement with no transactions for an account with no transactions', () => {
-      const account = new BankAccount();
       expect(account.printStatement()).toEqual('date || credit || debit || balance');
     })
 
     it('displays the correct bank statement for a single deposit', () => {
-      const account = new BankAccount();
       account.deposit('10/01/2023',1000);
       expect(account.printStatement()).toEqual('date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00')
     })
     
     it('displays the correct bank statement for a different deposit', () => {
-      const account = new BankAccount();
       account.deposit('11/02/2022',500.21);
       expect(account.printStatement()).toEqual('date || credit || debit || balance\n11/02/2022 || 500.21 || || 500.21')
     })
 
     it('displays the correct statement for two deposits', () => {
-      const account = new BankAccount();
       account.deposit('10/01/2023',1000);
       account.deposit('13/01/2023',2000);
       expect(account.printStatement()).toEqual('date || credit || debit || balance\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00')
     })
 
     it('displays the correct statement for two deposits and one withdrawal', () => {
-      const account = new BankAccount();
       account.deposit('10/01/2023',1000);
       account.deposit('13/01/2023',2000);
       account.withdraw('14/01/2023',500);
@@ -36,7 +37,6 @@ describe('BankAccount', () => {
     })
 
     it('displays the correct statement for three transactions entered in the wrong order', () => {
-      const account = new BankAccount();
       account.deposit('13/01/2023',2000);
       account.deposit('10/01/2023',1000);
       account.withdraw('14/01/2023',500);
@@ -44,7 +44,6 @@ describe('BankAccount', () => {
     })
 
     it('displays the correct statement for three different transactions', () => {
-      const account = new BankAccount();
       account.withdraw('31/01/2023',0.31);
       account.deposit('01/01/2023',500.31);
       account.withdraw('14/02/2023',500);
